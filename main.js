b83cf09ca77e816d1329ca61ff9d20643e00e463
Maël Acier <mael.acier@protonmail.com>
2018-03-31 22:54:32 +0200

    added last-fm

--- main.js
+++ main.js
@@ -128,3 +128,16 @@ function myFunction(){
         console.log(id,'UNDERSINGLE',index[id]);
     }
 }
+
+const LastFM = require('last-fm')
+const lastfm = new LastFM('e01234609d70b34055b389734707ac0a', { userAgent: 'MyApp/1.0.0 (http://example.com)' })
+
+/*lastfm.trackSearch({ q: 'Safe and Sound' }, (err, data) => {
+  if (err) console.error(err)
+  else console.log(data)
+})*/
+
+lastfm.artistInfo({ name: 'Daft Punk' }, (err, data) => {
+  if (err) console.error(err)
+  else console.log(data)
+})
