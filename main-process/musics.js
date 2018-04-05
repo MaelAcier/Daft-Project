b3b90f99de226f14c4507ac0551cc6493bfa7cc3
Maël Acier <mael.acier@protonmail.com>
2018-04-05 23:14:23 +0200

    error support + summary

--- main-process/musics.js
+++ main-process/musics.js
@@ -1,12 +1,20 @@
 const os = require('os');
 const fs = require('fs');
 const glob = require('glob');
+const https = require('https');
+const LastFM = require('last-fm')
 const ffmetadata = require ("ffmetadata");
 const electron = require('electron')
 const ipc = require('electron').ipcMain;
 const {dialog, app} = require('electron');
-const lastfm = require('./last-fm.js');
 
+const lastfm = new LastFM('e01234609d70b34055b389734707ac0a')
+const temp = `${os.tmpdir()}\\${electron.app.getName()}-${electron.app.getVersion()}`;
+var directory = [], error = [], index = {}, summary = {}, folder, loading;
+
+
+createDir(temp);
+console.log('tmp',temp);
 
 ipc.on('open-file-dialog', (event,args)=> {
   dialog.showOpenDialog({
@@ -28,13 +36,7 @@ ipc.on('open-file-dialog', (event,args)=> {
 });
 
 ipc.on('submit', (event)=> {
-      /*console.log('artists:',index);
-        console.log('artists2:',index["Justice"]);
-        console.log('single',Object.keys(index));
-        for (var id in index) { // On stocke l'identifiant dans « id » pour parcourir l'objet « family »
-            console.log(id,'UNDERSINGLE',index[id]);
-        }*/
-        console.log(summary);
+        //console.log(summary);
         newFolder = folder.split('\\');
         console.log(newFolder);
         newFolder.pop();
@@ -42,17 +44,42 @@ ipc.on('submit', (event)=> {
         writeMusics(folder);
 });
 
-ipc.on('summary', (artist,bio)=> {
-    summary[artist] = bio;
-});
+function artistRequest (artist) {   
+  lastfm.artistInfo({ name: artist }, (err, data) => {
+    if (err) console.error(err)
+    else {
+      console.log('data');
+      summary[artist] = data.summary;
+      download(data.images[data.images.length-1],`${temp}\\${artist}.png`);
+    }
+  });
+}
 
- 
-var directory = [], index = {}, summary = {}, folder;
+//artistRequest('Daft Punk');
+//albumRequest('Random Access Memories', 'Daft Punk');
 
-const temp = `${os.tmpdir()}\\${electron.app.getName()}-${electron.app.getVersion()}`;
-createDir(temp);
-console.log('tmp',temp);
 
+function albumRequest (album, artist) {
+  lastfm.albumInfo({ name: album , artistName: artist}, (err, data) => {
+    if (err) console.error(err)
+    else {
+      //console.log(data);
+      download(data.images[data.images.length-1],`${temp}\\${artist}\\${album}.png`);
+    }
+  })
+}
+
+
+
+function download (url, dest) {
+  var file = fs.createWriteStream(dest);
+  var request = https.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close();
+    });
+  });
+}
 
 function listFiles(dir){
   //directory = glob.sync(path.join(dir, '**/@(*.mp3|*.wav)'))
@@ -60,13 +87,17 @@ function listFiles(dir){
 }
 
 function listMusics (list){
+  var advancement = 0;
   list.forEach((file)=>{
       ffmetadata.read(file, (err,data)=>{
-        if (err) console.error("Error reading metadata", err);
+        if (err) {
+          console.error("Error reading metadata", err);
+          error.push(file);
+        }
         else {
           if (!Object.keys(index).includes(data.album_artist)){
             index[data.album_artist]={};
-            summary[data.album_artist] = lastfm.artist(data.album_artist);
+            artistRequest(data.album_artist);
           }
           if (!Object.keys(index[data.album_artist]).includes(data.album)){
             index[data.album_artist][data.album]={}; 
@@ -74,13 +105,20 @@ function listMusics (list){
             let coverpath = `${temp}\\${data.album_artist}`
             createDir(coverpath);
             ffmetadata.read(file, {coverPath: [`${coverpath}\\${data.album}.jpg`]}, (err)=>{
-              if (err) console.error("Error writing cover art");
+              if (err) {
+                console.error("Error writing cover art");
+                albumRequest(data.album, data.album_artist);
+              }
               else console.log("Cover art added");
             });
           }
           index[data.album_artist][data.album][data.track]={};
           index[data.album_artist][data.album][data.track].title=data.title;
           index[data.album_artist][data.album][data.track].path=file;
+          advancement++;
+          //console.log(advancement);
+          loading = advancement*100/directory.length;
+          console.log(`${loading}%`);
         }
       });
   });
