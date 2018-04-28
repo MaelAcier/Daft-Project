93ea6d77f7ead02aaf797abffaa0f44ec52b95f4
Maël Acier <mael.acier@protonmail.com>
2018-04-28 09:05:10 +0200

    export

---
+++ export/notification.js
@@ -0,0 +1,36 @@
+const notifier = require('node-notifier');
+const path = require('path');
+const {app} = require('electron')
+ 
+
+ 
+
+
+
+app.on('ready', () => {
+  notifier.notify({
+    title: 'My notification',
+    message: 'Hello, there!'
+  });
+  notifier.notify(
+    {
+      title: 'My awesome title',
+      message: 'Hello from node, Mr. User!',
+      icon: path.join(__dirname, 'daft.jpg'), // Absolute path (doesn't work on balloons)
+      sound: true, // Only Notification Center or Windows Toasters
+      wait: true // Wait with callback, until user action is taken against notification
+    },
+    function(err, response) {
+      // Response is response from notification
+    }
+  );
+})
+
+
+notifier.on('click', function(notifierObject, options) {
+  // Triggers if `wait: true` and user clicks notification
+});
+ 
+notifier.on('timeout', function(notifierObject, options) {
+  // Triggers if `wait: true` and notification closes
+});
