87e7a017ed6fcf3f422a93aae87a9625b491c4dd
Maël Acier <mael.acier@protonmail.com>
2018-05-10 21:05:00 +0200

    clear everything

--- renderer-process/home.js
+++
@@ -1,7 +0,0 @@
-const welcome = document.getElementById('welcome')
-
-
-welcome.addEventListener('click', function (event) {
-    welcome.innerHTML += `test`
-    document.getElementById('trigger-modal-full-split').click()
-  })
